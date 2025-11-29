dcb3bc131868314198e82189dcbe91923179677b
JamesParkerStrand <78117256+JamesParkerStrand@users.noreply.github.com>
2025-11-29 01:05:01 -0800

    Add files via upload

--- public/account.js
+++ public/account.js
@@ -16,6 +16,37 @@ async function getHouseholds() {
         const response = await fetch(`/users/viewhouseholds?user_id=${userId}`);
         const data = await response.json();
 
+        //for loop it for options
+        console.log(data[0].household_id);
+
+        getPets(data[0].household_id);
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function getPets(houseHoldId) {
+    try {
+        const response = await fetch(`/users/viewPets?household_id=${houseHoldId}`);
+        const data = await response.json();
+
+        //for loop it for options
+        console.log(data);
+        console.log(data[0].pet_id);
+        viewActivities(data[0].pet_id);
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function viewActivities(petId) {
+    try {
+        const response = await fetch(`/users/viewNotesAndActivities?pet_id=${petId}`);
+        const data = await response.json();
+
+        //for loop it for options
         console.log(data);
 
     } catch (err) {
